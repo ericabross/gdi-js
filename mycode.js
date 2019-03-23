3b4117149bb7c7dd0f0acabeaec7ce6cf5c1f6a4
thisismynightmare <ericabross@gmail.com>
2019-03-22 22:12:52 -0500

    created tip calculation

--- mycode.js
+++ mycode.js
@@ -1,9 +1,28 @@
 alert('Hello World!'); // practice comment
 console.log('Secret message');
 
+//variable practice
+
 var rhobhCast = 6
 
 var rhoaCast = 6
 var rhonyCast = 5
 
 var totalRH = rhoaCast + rhonyCast;
+
+
+//strings practice
+var firstName = 'Erica';
+
+var fullName = firstName + ' Ross';
+
+
+//variables and strings - tip calculation
+var billPretip = 15;
+var tipPercentage = .20;
+var tip = billPretip * tipPercentage;
+var totalBill = billPretip + tip;
+totalBill = totalBill.toFixed (2);
+
+
+var outputBill = 'Your total bill, with tip, is ' + '$' + totalBill;
